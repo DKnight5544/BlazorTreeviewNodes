bdf112427f3d8cdd68a5d7162e42659dd06a4900
David Knight <dave@mostlyspam.com>
2021-09-22 17:02:14 -0700

    Initial Commit

---
+++ BlazorMinimal/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorMinimal.Pages
+{
+    public partial class Index
+    {        
+        string title = "Client-side (Web Assembly) Blazor Minimal";
+        string GetTextFromMethodInClass()
+        {
+            return "This text is derived from a C# .cs file within the assembly";
+        }
+
+    }
+}
